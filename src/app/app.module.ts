df5a5e4f27c8925987fc4c052c802ca608ba5fdb
DEV <DEV@10.100.0.100>
2024-02-17 14:23:00 -0300

    Recuperando arquivos corrompidos package-lock e package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './components/game/game.component';
-import { ValidatedGameDirective } from './shared/directives/validated-game.directive';
 import { ButtonGameDirective } from './shared/directives/button-game.directive';
-import { DialogComponent } from './shared/components/dialog/dialog.component';
-
+import { ValidatedGameDirective } from './shared/directives/validated-game.directive';
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     ValidatedGameDirective,
-    ButtonGameDirective,
-    DialogComponent
+    ButtonGameDirective
   ],
   imports: [
     BrowserModule,
